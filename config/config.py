4d2eeceabe927cfdc1c99e2c0d3cd389578040cb
md <michael.dammann.compsci@gmail.com>
2022-03-04 13:44:22 +0100

    Initialized first set of configs.

---
+++ config/config.py
@@ -0,0 +1,24 @@
+'''
+SHAPES: List of what kind of objects can occur in the images
+implemented: "CIRLCE", "RECTANGLE" not yet implemented: "ANNULUS", "ARROW", "ELLIPSE", "POLYGON",
+'''
+SHAPES = ["CIRCLE", "RECTANGLE"]
+
+'''
+COLORS: List of colors the occuring objects can have
+implemented: "RED", "GREEN"
+'''
+COLORS = ["RED", "GREEN"]
+
+'''
+OVERLAP: Are occuring objects allowed to overlap? 
+implemented: False, not yet implemented: True
+'''
+OVERLAP = False
+
+'''
+IMG_HEIGHT, IMG_WIDTH: Dimension of the generated images
+'''
+
+IMG_HEIGHT= 150
+IMG_WIDTH = 150
