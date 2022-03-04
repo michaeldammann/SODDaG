f5fb07652990c06de28feaff30534dacb10feab8
md <michael.dammann.compsci@gmail.com>
2022-03-04 16:13:36 +0100

    First simple images.

--- config/config.py
+++ config/config.py
@@ -5,14 +5,16 @@ Global configurations
 SHAPES = ["CIRCLE", "RECTANGLE"]
 '''
 SHAPES: List of what kind of objects can occur in the images.
-Implemented: "CIRLCE", "RECTANGLE" not yet implemented: "ANNULUS", "ARROW", "ELLIPSE", "POLYGON",
+Implemented: "CIRCLE", "RECTANGLE" not yet implemented: "ANNULUS", "ARROW", "ELLIPSE", "POLYGON",
 '''
 
-COLORS = ["RED", "GREEN"]
+COLORS = ["red", "green", "blue", "black", "grey", "pink"]
 '''
 COLORS: List of colors the occurring objects can have.
 Implemented: "RED", "GREEN"
 '''
+
+EDGECOLORS = ["red", "green", "blue", "black", "grey", "white"]
 
 OVERLAP = False
 '''
@@ -20,13 +22,13 @@ OVERLAP: Are occurring objects allowed to overlap?
 Implemented: False, not yet implemented: True
 '''
 
-IMG_HEIGHT= 150
+IMG_HEIGHT= 15
 '''
-IMG_HEIGHT: Dimension of the generated images
+IMG_HEIGHT: "Matplotlib Dimension" of the generated images
 '''
-IMG_WIDTH = 150
+IMG_WIDTH = 15
 '''
-IMG_WIDTH: Dimension of the generated images
+IMG_WIDTH: "Matplotlib Dimension" of the generated images
 '''
 
 N_OBJECTS_MIN=1
@@ -37,3 +39,17 @@ N_OBJECTS_MAX=1
 '''
 N_OBJECTS_MAX: Maximum number of objects that can occur in one image
 '''
+
+MIN_SIZE = 0.5
+'''
+MIN_SIZE: Minimal size of object in "Matplotlib Dimensions" 
+'''
+
+MAX_SIZE = 6.0
+'''
+MAX_SIZE: Maximum size of object in "Matplotlib Dimensions" 
+'''
+
+SHOW_FRAME = True
+'''
+SHOW_FRAME: Whether to show black frame around image'''
