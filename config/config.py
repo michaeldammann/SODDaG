b6d7b9b8ab4ee5bf06c902bc3f2b1338ff7138ab
md <michael.dammann.compsci@gmail.com>
2022-03-04 13:54:35 +0100

    Configs style fix.

--- config/config.py
+++ config/config.py
@@ -1,29 +1,39 @@
 '''
-SHAPES: List of what kind of objects can occur in the images
-implemented: "CIRLCE", "RECTANGLE" not yet implemented: "ANNULUS", "ARROW", "ELLIPSE", "POLYGON",
+Global configurations
 '''
+
 SHAPES = ["CIRCLE", "RECTANGLE"]
+'''
+SHAPES: List of what kind of objects can occur in the images.
+Implemented: "CIRLCE", "RECTANGLE" not yet implemented: "ANNULUS", "ARROW", "ELLIPSE", "POLYGON",
+'''
 
-'''
-COLORS: List of colors the occurring objects can have
-implemented: "RED", "GREEN"
-'''
 COLORS = ["RED", "GREEN"]
+'''
+COLORS: List of colors the occurring objects can have.
+Implemented: "RED", "GREEN"
+'''
 
+OVERLAP = False
 '''
 OVERLAP: Are occurring objects allowed to overlap? 
-implemented: False, not yet implemented: True
+Implemented: False, not yet implemented: True
 '''
-OVERLAP = False
 
-'''
-IMG_HEIGHT, IMG_WIDTH: Dimension of the generated images
-'''
 IMG_HEIGHT= 150
+'''
+IMG_HEIGHT: Dimension of the generated images
+'''
 IMG_WIDTH = 150
+'''
+IMG_WIDTH: Dimension of the generated images
+'''
 
-'''
-N_OBJECTS_MIN, N_OBJECTS_MAX: Minimum and maximum number of objects that can occur in one image
-'''
 N_OBJECTS_MIN=1
+'''
+N_OBJECTS_MIN: Minimum number of objects that can occur in one image
+'''
 N_OBJECTS_MAX=1
+'''
+N_OBJECTS_MAX: Maximum number of objects that can occur in one image
+'''
